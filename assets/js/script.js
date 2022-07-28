b960d5f364bd7182c3a2583925601aa5b1a4e83b
Josh Rae <joshuar2155@gmail.com>
2022-07-27 20:22:22 -0400

    date and time testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
     // moment.js
+    var timeDisplayEl = $('#time-display');
+
+    function displayTime() {
+        var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+        timeDisplayEl.text(rightNow);
+      }   
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 // WHEN I view the timeblocks for that day
@@ -12,3 +18,5 @@
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
+
+setInterval(displayTime, 1000);
